35f1473dba0f19e119210354eff95aa3c5e13cab
philip <philip.clarke@redhood.co.uk>
2015-11-17 12:32:52 +0000

    one test failing

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,20 @@
 class Order
 
-  attr_reader :order
+  attr_reader :order, :total_price, :menu
+
   def initialize
+    @menu = Menu.new
     @order =[]
+    @total_price = 0
   end
 
   def add_item(item)
-    order << item
+    order << menu.menu[item]
+  end
+
+  def create_total
+    order.inject(:+)
+
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,8 +1,9 @@
 require 'order'
 
 describe Order do
-  let(:menu_item1) {double :menu_item1, name: "Cafe Latte", price: 4.75}
 
+  item1 = "Cafe Latte"
+  item2 = "Tea"
   it 'can contain menu items as an array' do
     expect(subject.order).to be_a Array
   end
@@ -12,5 +13,11 @@ describe Order do
     expect(subject.order.count).to eq(1)
   end
 
+  it 'can add two items together' do
+    subject.add_item(item1)
+    subject.add_item(item2)
+    subject.create_total
+    expect(subject.total_price).to eq(8.40)
+  end
 
 end
